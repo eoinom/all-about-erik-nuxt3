8a5b819fc41296843cf38455efa1d10ee4a7cf3b
Eoin <eoinom@gmail.com>
2023-01-25 01:13:09 +0000

    Add bootstrap-vue (early nuxt3 version)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content'],
-  css: ['@/assets/styles.css', 'simplebar/dist/simplebar.min.css'],
+  modules: [
+    '@nuxt/content',
+    [
+      'bootstrap-vue-next/nuxt',
+      {
+        components: ['BContainer', 'BRow', 'BCol', 'BButton'],
+        directives: ['VBTooltip'],
+      },
+    ],
+  ],
+  css: [
+    '@/assets/styles.css',
+    'simplebar/dist/simplebar.min.css',
+    'bootstrap/dist/css/bootstrap.css',
+    '@/assets/custom.scss', // bootstrap-vue overrides
+  ],
 });
